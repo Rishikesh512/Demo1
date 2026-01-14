d8f3cc94a906f4614a3d581d2d3a594d707a29b3
Rishikesh512 <rishikeshsamudre1@gmail.com>
2026-01-14 17:03:57 +0530

    first demo

---
+++ demo.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+    int n;
+    printf("Enter a number:");
+    scanf("%d",&n);
+    if (n%2==0)
+    {
+      printf("The number is even");
+    }else("the number is odd");
+    
+    
+    return 0;
+}
